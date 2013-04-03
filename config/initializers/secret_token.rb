aadb74003b658d9a4c6dba02385558b7cf7dd260
jackieniko <jacqulynnichole@gmail.com>
2013-04-03 11:33:14 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogbrandconnect::Application.config.secret_token = 'dc84af001f500968c0c1d75fc980af7fc17302248e2a83b9cf37814e24680ee6b4921d7b98533c32119361d5ee4f93f2acb4a67369abdc3681fb84645e3b1b1b'
